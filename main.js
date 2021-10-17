a4b489c6cc6103302613efaea68f754f6e4250b0
DeathStinger <87364785+DeathStinger@users.noreply.github.com>
2021-10-17 18:50:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+img ="";
+function preload() {
+  img=loadImage('dog_cat.jpg')  
+}
+
+function setup() {
+   canvas=creatCanvas(640,480);
+   canvas.center();
+}
+function bedroom() {
+ window.location = "bedroom.html";
+}
+function bottles() {
+  window.location = "bottles.html";
+ }
+ function desk() {
+  window.location = "desk.html";
+ }
+ function fruit_basket() {
+  window.location = "fruit_basket.html";
+ }
+ function TV() {
+  window.location = "TV.HTML";
+ }
